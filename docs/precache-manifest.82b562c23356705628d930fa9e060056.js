cd87febbed3f4e9cff6d1cb15a7425a02e16124e
Fatmir87 <56822851+Fatmir87@users.noreply.github.com>
2020-09-15 10:01:44 +0200

    Update

---
+++ docs/precache-manifest.82b562c23356705628d930fa9e060056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd093fbdc13dea7871971b53345f4e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be7540da96b8be981927",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a4fe22693fc40c68044c",
+    "url": "/static/js/2.bed56618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bed56618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7540da96b8be981927",
+    "url": "/static/js/main.060391e2.chunk.js"
+  },
+  {
+    "revision": "636422c4c007d1f12d38",
+    "url": "/static/js/runtime-main.14ca8da4.js"
+  }
+]);
